679e0af473413a78674e3d11e009dcec8cd87867
hyeonu577 <hyeonu@snu.ac.kr>
2026-02-12 17:46:26 +0900

    delete .sh

--- snuphya.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOG_FILE="/home/pi/snuphya/snuphya.log"
-
-echo "$(date +'%Y-%m-%d %H:%M:%S') Starting snuphya" >> "$LOG_FILE"
-python /home/pi/snuphya/main.py >> "$LOG_FILE" 2>&1
-if [ $? -ne 0 ]; then
-    echo "$(date +'%Y-%m-%d %H:%M:%S') [ERROR] snuphya script execution failed!" >> "$LOG_FILE"
-fi
